18c967702c62dca22715820b64cb8135991ac415
李谊鑫 <yixinli4@creditease.cn>
2019-09-24 23:16:40 +0800

    refactor

---
+++ src/com/practice/leetcode/sourcecode/HashMapSource.java
@@ -0,0 +1,11 @@
+package com.practice.leetcode.sourcecode;
+
+import java.util.HashMap;
+
+public class HashMapSource {
+    public static void main(String[] args) {
+        
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("", "");
+    }
+}
